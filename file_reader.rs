1b510e2ccbda54ec33f450427cfcb713bce7e1c4
Yoichi Hirai <i@yoichihirai.com>
2013-12-08 18:56:39 +0900

    shallower

--- file_reader.rs
+++ file_reader.rs
@@ -13,13 +13,11 @@ fn main() {
             if (f.tell() < (2 * size_of::<u64>()) as u64) {
                 fail!()
             }
-            else{
-                f.seek(0, SeekSet);
-                let num0 = f.read_be_u64();
-                let num1 = f.read_be_u64();
-                println(num0.to_str());
-                println(num1.to_str())
-            }
+            f.seek(0, SeekSet);
+            let num0 = f.read_be_u64();
+            let num1 = f.read_be_u64();
+            println(num0.to_str());
+            println(num1.to_str())
         } // file closed???
     }
 }
