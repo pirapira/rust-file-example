b83619e7eebb5fb4af8be1418320c3b589d7dc42
Yoichi Hirai <i@yoichihirai.com>
2013-12-09 22:09:19 +0900

    shallower indents, thx atuer http://www.reddit.com/r/rust/comments/1sdnmx/a_pair_of_rust_programs_that_write_to_and_read/cdwnk7a

--- file_reader.rs
+++ file_reader.rs
@@ -5,18 +5,15 @@ use std::mem::size_of;
 fn main() {
     let f = File::open_mode(&Path::new("./test.dat"), Open, Read);
 
-    match f {
-        None => fail!(),
-        Some(mut f) => {
-            f.seek(0, SeekEnd);
-            if f.tell() < (2 * size_of::<u64>()) as u64 {
-                fail!()
-            }
-            f.seek(0, SeekSet);
-            let num0 = f.read_be_u64();
-            let num1 = f.read_be_u64();
-            println(num0.to_str());
-            println(num1.to_str())
-        } // file closed
+    let mut f = f.expect("open_mode failed.");
+
+    f.seek(0, SeekEnd);
+    if f.tell() < (2 * size_of::<u64>()) as u64 {
+      fail!()
     }
+    f.seek(0, SeekSet);
+    let num0 = f.read_be_u64();
+    let num1 = f.read_be_u64();
+    println(num0.to_str());
+    println(num1.to_str())
 }
--- file_writer.rs
+++ file_writer.rs
@@ -2,17 +2,14 @@ use std::io::fs::File;
 use std::io::{Write, Truncate};
 
 fn main() {
-    let f =
-        File::open_mode(&Path::new("./test.dat"), Truncate, Write);
+  let f =
+      File::open_mode(&Path::new("./test.dat"), Truncate, Write);
 
-    let num0 : u64 = 90;
-    let num1 : u64 = 22;
+  let num0 : u64 = 90;
+  let num1 : u64 = 22;
 
-    match f {
-        None => fail!(),
-        Some(mut f) => {
-            f.write_be_u64(num0);
-            f.write_be_u64(num1)
-        } // file closed
-    }
+  let mut f = f.expect("open_mode failed.");
+
+  f.write_be_u64(num0);
+  f.write_be_u64(num1)
 }
